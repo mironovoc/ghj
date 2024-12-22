e0081e2a64dd6a2bf938c4be895e02eb717d47c9
Oleg <mironbrrr1994@gmail.com>
2024-12-22 18:46:19 +0300

    2

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -0,0 +1,60 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+    private final UserServiceImp userService;
+
+    @Autowired
+    public UserRestController(UserServiceImp userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.allUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.findUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userService.findUserById(id);
+        if (existingUser != null) {
+            user.setId(id);
+            userService.saveUser(user);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+}
